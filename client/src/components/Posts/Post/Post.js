844031f858c646f93609caf5411881f53f2628c6
David Chan <dchan902@gmail.com>
2022-07-02 09:32:25 -0600

    import material ui componenents to post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
-
+import { Card, CardActions, CardContent, CardMedia, Button, Typography} from '@material-ui/core'
+import ThumbUpAltIcon from '@material-ui/core'
 
 import useStyles from './styles';
 
 const Post = () => {
-  const dispatch = useDispatch();
   const classes = useStyles();
   return (
     <h1>POST</h1>
