78ee66de6786336a3c8d9978fe7fa3aa0dc7a24c
David Chan <dchan902@gmail.com>
2022-07-03 15:55:43 -0600

    add clear function call to handleSubmit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -26,8 +26,10 @@ const Form = ({ currentId, setCurrentId }) => {
     
     if(currentId) {
       dispatch(updatePost(currentId, postData));
+      clear();
     } else {
       dispatch(createPost(postData));
+      clear();
     }
 
   }
