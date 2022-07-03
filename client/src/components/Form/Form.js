1eb77ef32541b768815f35f5430f63b673f238f2
David Chan <dchan902@gmail.com>
2022-07-02 21:32:10 -0600

    fix bug and import updatePost from controller to posts routes

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper} from '@material-ui/core'
 import FileBase from 'react-file-base64'
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
@@ -13,8 +13,13 @@ const Form = ({ currentId, setCurrentId }) => {
     tags: '',
     selectedFile:''
   })
+  const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
   const classes = useStyles();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(post) setPostData(post)
+  }, [post]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
