22aa1446cd415780a66f20d81c611eb7da03e339
David Chan <dchan902@gmail.com>
2022-07-02 21:06:55 -0600

    add updatePost action

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,4 +4,4 @@ const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost)
-export const updatePost = (id, updaatedPost) => axios.patch(`${url}/${id}`);
+export const updatePost = (id, updaatedPost) => axios.patch(`${url}/${id}`, updaatedPost);
