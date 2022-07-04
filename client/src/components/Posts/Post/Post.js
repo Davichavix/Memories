880330456a09b1f0211fd05e8a60baa95ae8b4b1
David Chan <dchan902@gmail.com>
2022-07-03 18:15:12 -0600

    implement delete logic and add to onclick button on post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,11 +4,15 @@ import Delete from '@material-ui/icons/Delete';
 import ThumbUpAlt from '@material-ui/icons/ThumbUpAlt';
 import MoreHoriz from '@material-ui/icons/MoreHoriz';
 import moment from 'moment'
+import { useDispatch } from 'react-redux';
+import { deletePost } from "../../../actions/posts";
 
 import useStyles from './styles';
 
 const Post = ( {post, setCurrentId}) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return (
    <Card className={classes.card}>
      <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
@@ -34,7 +38,7 @@ const Post = ( {post, setCurrentId}) => {
         Like
         {post.likeCount}
       </Button>
-      <Button size="small" color="primary" onClick={() => {}}>
+      <Button size="small" color="primary" onClick={() => {dispatch(deletePost(post._id))}}>
         <Delete fontSize="small" />
         Delete
       </Button>
