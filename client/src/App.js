e491867926c0be3161fcd613aec6578b29c988a7
David Chan <dchan902@gmail.com>
2022-07-03 19:45:45 -0600

    add mobile responsiveness with form at top of app

--- client/src/App.js
+++ client/src/App.js
@@ -25,14 +25,14 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId}/>
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId}/>
             </Grid>
-          </Grid>
+          </Grid>s
         </Container>
       </Grow>
     </Container>
