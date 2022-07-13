3c474593b2b61adb336f271a61296f837cb46887
David Chan <dchan902@gmail.com>
2022-07-12 21:47:15 -0600

    add coinData controller logic for fetching coin data

---
+++ server/controllers/coinData.js
@@ -0,0 +1,14 @@
+
+
+export const getCoinData = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const URL = `https://api.coingecko.com/api/v3/coins/${id}?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true`;
+
+    const { data } = await axios.get(URL)
+
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(404).json({ message: error.message})
+  }
+}
